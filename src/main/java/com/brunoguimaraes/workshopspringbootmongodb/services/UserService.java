91ba7f85172732851517a033492002316eb7ab33
Bruno Guimarães <brunoelinton@outlook.com>
2022-04-13 17:15:17 -0300

    Obtendo um usuário por id

--- src/main/java/com/brunoguimaraes/workshopspringbootmongodb/services/UserService.java
+++ src/main/java/com/brunoguimaraes/workshopspringbootmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.brunoguimaraes.workshopspringbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brunoguimaraes.workshopspringbootmongodb.domain.User;
 import com.brunoguimaraes.workshopspringbootmongodb.repository.UserRepository;
+import com.brunoguimaraes.workshopspringbootmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,11 @@ public class UserService {
 	// RETURN ALL USERS
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	// RETURN USER BY ID
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);		
+		return user.orElseThrow(()-> new ObjectNotFoundException("Object not found."));
 	}
 }
