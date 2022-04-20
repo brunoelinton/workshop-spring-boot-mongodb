c60194dfd9a4a8f12076a322ceb323739b143baf
Bruno Guimarães <brunoelinton@outlook.com>
2022-04-20 10:51:11 -0300

    Simple query with query methods

--- src/main/java/com/brunoguimaraes/workshopspringbootmongodb/services/PostService.java
+++ src/main/java/com/brunoguimaraes/workshopspringbootmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.brunoguimaraes.workshopspringbootmongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +22,8 @@ public class PostService {
 		return post.orElseThrow(()-> new ObjectNotFoundException("Object not found."));
 	}
 
+	// RETURN A POST WITH TEXT
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
+	}
 }
