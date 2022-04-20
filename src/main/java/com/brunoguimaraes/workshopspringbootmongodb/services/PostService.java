79529c11317172eb43e330a1fdf55dfd9af8c4fd
Bruno Guimarães <brunoelinton@outlook.com>
2022-04-19 23:35:12 -0300

    Getting a post by id

---
+++ src/main/java/com/brunoguimaraes/workshopspringbootmongodb/services/PostService.java
@@ -0,0 +1,24 @@
+package com.brunoguimaraes.workshopspringbootmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brunoguimaraes.workshopspringbootmongodb.domain.Post;
+import com.brunoguimaraes.workshopspringbootmongodb.repository.PostRepository;
+import com.brunoguimaraes.workshopspringbootmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+		
+	// RETURN USER BY ID
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);		
+		return post.orElseThrow(()-> new ObjectNotFoundException("Object not found."));
+	}
+
+}
